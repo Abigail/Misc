232fa115ff119d59f9bd7b4cd8d9cbaab7a67167
Abigail <abigail@abigail.be>
2021-06-18 16:44:41 +0200

    Add an input loop

--- Templates/Challenge/template.java
+++ Templates/Challenge/template.java
@@ -6,8 +6,20 @@
 // Run as: ln ch-1.java ch1.java; javac ch1.java; java ch1 < input-file
 //
 
+import java.util.*;
+
 public class ch1 {
     public static void main (String [] args) {
-
+        Scanner scanner = new Scanner (System . in);
+        try {
+            while (true) {
+                String line = scanner . nextLine ();
+            }
+        }
+        catch (Exception e) {
+            //
+            // EOF
+            //
+        }
     }
 }
