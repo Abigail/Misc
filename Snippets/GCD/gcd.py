952477f5bc588c36868105c2a1e6929020c91f72
Abigail <abigail@abigail.be>
2021-10-26 13:39:44 +0200

    Python and Ruby snippets for GCD

---
+++ Snippets/GCD/gcd.py
@@ -0,0 +1,7 @@
+import math
+
+#
+# Just reminder; we would never copy-and-paste this snippet
+#
+def gcd (m, n):
+    math . gcd (m, n)
---
+++ Snippets/GCD/gcd.rb
@@ -0,0 +1,6 @@
+#
+# Just reminder; we would never copy-and-paste this snippet
+#
+def gcd (m, n)
+    return m . gcd (n)
+end
