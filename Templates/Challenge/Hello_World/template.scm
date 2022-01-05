b49f58fb5130c2096d9109e02b476b94ba5eab61
Abigail <abigail@abigail.freedom.nl>
2022-01-05 18:39:12 +0100

    Hash bang line for guile

--- Templates/Challenge/Hello_World/template.scm
+++ Templates/Challenge/Hello_World/template.scm
@@ -1,3 +1,6 @@
+#!/usr/local/bin/guile
+!#
+
 ;;;
 ;;; See https://theweeklychallenge.org/blog/perl-weekly-challenge-000
 ;;;
--- Templates/Challenge/template.scm
+++ Templates/Challenge/template.scm
@@ -1,3 +1,6 @@
+#!/usr/local/bin/guile
+!#
+
 ;;;
 ;;; See https://theweeklychallenge.org/blog/perl-weekly-challenge-000
 ;;;
@@ -8,6 +11,7 @@
 
 
 (use-modules (ice-9 rdelim))
+
 (define (main)
     (define line (read-line))
     (if (not (eof-object? line))
